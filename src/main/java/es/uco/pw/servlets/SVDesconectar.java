9f8553efde4e8c82eba6a9897b218d73f3f58784
Alex Merina <Alex Merina@192.168.1.128>
2023-12-16 18:56:28 +0100

    igual que antes

--- src/main/java/es/uco/pw/servlets/SVDesconectar.java
+++ src/main/java/es/uco/pw/servlets/SVDesconectar.java
@@ -1,18 +1,23 @@
 
-
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class SVDesconectar
  */
 @WebServlet("/DesconectarServlet")
-public class DesconectarServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+public class SVDesconectar extends HttpServlet {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1374157113522829569L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // Invalidar la sesión actual
         HttpSession session = request.getSession(false);
